6d3becfb3aff005ae845fb7932c8c1ccdd53771a
Sloan <sloan0158@gmail.com>
2023-08-03 17:23:35 +0900

    제곱추가

--- main.py
+++ main.py
@@ -5,6 +5,16 @@ def add_func(n1,n2):
 def sub_func(n1,n2):
     return n1-n2
 
+def multip_func(n1,n2):
+    return n1*n2
+
+def div_func(n1,n2):
+    return n1/n2
+
+def sq_func(n1,n2):
+    return n1^n2
+
+
 ##전역 변수부
 num1,num2 = 100,200
 result=0
@@ -15,3 +25,12 @@ print(num1,'+',num2,'=',result)
 
 result=sub_func(num1,num2)
 print(num1,'-',num2,'=',result)
+
+result=multip_func(num1,num2)
+print(num1,'*',num2,'=',result)
+
+result=div_func(num1,num2)
+print(num1,'/',num2,'=',result)
+
+result=sq_func(num1,num2)
+print(num1,'^',num2,'=',result)
